badeab884789e1db356e7fd3d4d49716128ea5a2
Vandyck <vandycklai@Vandycks-MacBook-Pro.local>
2023-03-15 17:57:03 +0800

    Overflow fixage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,149 +18,152 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey[200],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.grey[200],
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-              // App Logo
-              const Icon(
-                Icons.flutter_dash,
-                size: 100,
-              ),
+                  // App Logo
+                  const Icon(
+                    Icons.flutter_dash,
+                    size: 100,
+                  ),
 
-              const SizedBox(
-                height: 50,
-              ),
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-              // App title
-              Text("Flutter Frenzy\nBootcamp",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
+                  // App title
+                  Text("Flutter Frenzy\nBootcamp",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
 
-              const SizedBox(
-                height: 25,
-              ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-              // username textfield
-              CustomTextField(
-                controller: usernameController,
-                hintText: 'Username',
-                obscureText: false,
-              ),
+                  // username textfield
+                  CustomTextField(
+                    controller: usernameController,
+                    hintText: 'Username',
+                    obscureText: false,
+                  ),
 
-              const SizedBox(height: 10),
+                  const SizedBox(height: 10),
 
-              // password textfield
-              CustomTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
+                  // password textfield
+                  CustomTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
 
-              const SizedBox(height: 10),
+                  const SizedBox(height: 10),
 
-              // forgot password?
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
+                  // forgot password?
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  ),
 
-              const SizedBox(height: 25),
+                  const SizedBox(height: 25),
 
-              // sign in button
-              CustomButton(
-                onTap: signUserIn,
-              ),
+                  // sign in button
+                  CustomButton(
+                    onTap: signUserIn,
+                  ),
 
-              const SizedBox(height: 50),
+                  const SizedBox(height: 50),
 
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
+                  // or continue with
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'Or continue with',
+                            style: TextStyle(color: Colors.grey[700]),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // google + apple sign in buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      // google button
+                      SquareTile(imagePath: 'assets/img/google.png'),
+
+                      SizedBox(width: 25),
+
+                      // apple button
+                      SquareTile(
+                        icon: Icons.apple,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
+                    ],
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // not a member? register now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a member?',
                         style: TextStyle(color: Colors.grey[700]),
                       ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
+                      const SizedBox(width: 4),
+                      const Text(
+                        'Register now',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // google + apple sign in buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  // google button
-                  SquareTile(imagePath: 'assets/img/google.png'),
-
-                  SizedBox(width: 25),
-
-                  // apple button
-                  SquareTile(
-                    icon: Icons.apple,
+                    ],
                   ),
+
+                  const SizedBox(height: 20),
                 ],
               ),
-
-              const SizedBox(height: 50),
-
-              // not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    'Register now',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              )
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
